8f007f5e77a4aa273ae098bd053c87273ff28ff8
Qian-Jiaxin <781153660@qq.com>
2021-03-06 15:49:57 +0800

    update

--- Developement_Code/python_controller/controller_base.py
+++ Developement_Code/python_controller/controller_base.py
@@ -2,7 +2,7 @@ import ctypes
 
 class Controller(object):
     def __init__(self):
-        self.__path='./MPC08D.dll'#按照集体路径更新
+        self.__path='./MPC08D.dll'#按照具体路径设置
         self.controller=ctypes.windll.LoadLibrary(self.__path)
     def auto_set(self):
         self.controller.auto_set.restype=ctypes.c_int
